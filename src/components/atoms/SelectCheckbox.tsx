ca7caa59493cd6c31bfc2d1000a19f02b7ef2a26
Abdul-Afiz <jimohafeez738@gmail.com>
2022-10-27 17:56:49 +0100

    updated text

---
+++ src/components/atoms/SelectCheckbox.tsx
@@ -0,0 +1,56 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import clsx from "clsx";
+import CaretDown from "./vectors/caret-down";
+
+const SelectCheckbox = ({
+  title,
+  twClasses,
+  icon,
+  placeholder,
+  lists,
+  onClick,
+}: {
+  title: string;
+  twClasses?: string;
+  icon?: any;
+  placeholder: string;
+  lists: Array<string>;
+  onClick: Dispatch<SetStateAction<string>>;
+}) => {
+  const [openSubmenu, setOpenSubmenu] = useState(false);
+  const handleOpenSubmenu = () => {
+    setOpenSubmenu((prev) => !prev);
+  };
+  const handleSelect = (params: string) => {
+    onClick(params);
+    setOpenSubmenu((prev) => !prev);
+  };
+
+  return (
+    <div className={clsx("relative", twClasses)}>
+      <div className="select" onClick={handleOpenSubmenu}>
+        <span className="flex space-x-6 items-center">
+          {icon} {title} : {placeholder}
+        </span>
+        <span
+          className={clsx("sidebar-toggle-btn", openSubmenu && "sidebar-open")}
+        >
+          <CaretDown />
+        </span>
+      </div>
+      <div className={clsx("select-lists", !openSubmenu ? "hidden" : "flex")}>
+        {lists.map((label, i) => (
+          <span
+            key={label + i}
+            className="py-[0.85rem] lg:py-4 px-2 lg:px-3 capitalize cursor-pointer hover:bg-bg-3 rounded-md"
+            onClick={() => handleSelect(label)}
+          >
+            {label}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SelectCheckbox;
