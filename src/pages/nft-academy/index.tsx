66a34af5b9f51f1e4a6e8f3f6030b729fc484ead
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2022-11-25 19:07:33 +0100

    nft-academy:swipe-2

--- src/pages/nft-academy/index.tsx
+++ src/pages/nft-academy/index.tsx
@@ -1,10 +1,14 @@
+import { useRef, useEffect, useState, LegacyRef } from "react";
 import { Button, Heading, Heading2 } from "@/src/components/atoms";
+import { PlayIcon } from "@/src/components/atoms/vectors";
 import { NftAcadCard, NftSlider } from "@/src/components/molecules";
 import { Footer } from "@/src/components/organisms";
 import DashboardLayout from "@/src/template/DashboardLayout";
 import Image from "next/image";
 
 const Index = () => {
+  const [show, setShow] = useState("");
+  const parentRef = useRef(null);
   const nftAcadData = [
     {
       imgUrl: "/images/blog-img.svg",
@@ -31,6 +35,40 @@ const Index = () => {
       name: "  Get Comfortable With The Basic",
     },
   ];
+
+  const nftAcadVid = [
+    { label: "How NFTs changed my life", img: "/images/cloudax-acad.svg" },
+    { label: "Learn Nfts", img: "/images/cloudax-acad.svg" },
+    { label: "NFTs changed my life", img: "/images/cloudax-acad.svg" },
+    { label: "NFTs changed life", img: "/images/cloudax-acad.svg" },
+  ];
+
+  const [cardRef] = useState(
+    Array(nftAcadVid.length).fill(useRef(<div></div>))
+  );
+
+  useEffect(() => {
+    const options = {
+      root: parentRef.current,
+      threshold: 0.8,
+    };
+
+    const major = [...cardRef];
+
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0].isIntersecting;
+      entry && setShow(entries[0].target.id);
+      console.log(entries[0]);
+    }, options);
+
+    major.forEach((item) => {
+      // console.log({ item });
+      //@ts-ignore
+      observer.observe(item);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <DashboardLayout>
       <div className="sub-layout-wrapper scrollbar-hide flex flex-col justify-between">
@@ -81,6 +119,48 @@ const Index = () => {
                 <span className="text-xl font-medium text-txt-2 block mb-10">
                   Hear from the NFT community on a variety of topics.
                 </span>
+              </div>
+              <div className="">
+                <div
+                  ref={parentRef}
+                  className="w-full flex overflow-x-auto snap-x scrollbar-hide"
+                >
+                  {nftAcadVid.map((val, i) => (
+                    <div
+                      className="relative h-[35rem] w-full shrink-0 snap-end"
+                      key={val.label + i}
+                      id={i.toString()}
+                      ref={cardRef[i]}
+                    >
+                      <Image
+                        src={val.img}
+                        alt={"acad-video" + i}
+                        placeholder="blur"
+                        blurDataURL="/images/placeholder.png"
+                        objectFit="cover"
+                        layout="fill"
+                        className="rounded-xl"
+                      />
+                      <div className="absolute bottom-[10%] flex justify-between items-center w-full px-12">
+                        <Heading2 title={val.label} />
+                        <span className="text-xl text-black font-bold flex items-center gap-x-2 bg-white rounded-xl py-4 px-6">
+                          <PlayIcon />
+                          Play
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div className="flex gap-5 justify-center mt-[5rem]">
+                  {nftAcadVid.map((_, i) => (
+                    <span
+                      key={i}
+                      // className={` transition-all delay-500 h-3 rounded-3xl ${
+                      //   show === i ? "bg-white w-28" : "bg-[#99a0ff5b] w-14"
+                      // }`}
+                    ></span>
+                  ))}
+                </div>
               </div>
             </section>
           </div>
