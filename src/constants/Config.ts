1d595bd3b5a22dfa18aa6dc2104ded08156d2bf9
Marcellus Nwankwo <marhandrez@gmail.com>
2022-12-01 07:33:12 +0100

    abi updated

--- src/constants/Config.ts
+++ src/constants/Config.ts
@@ -1,6 +1,7 @@
 const APPCONFIG = {
   DEFAULT_ITEM_ART: "/assets/images/defaults/song_art.png",
   DEFAULT_USER_AVATAR: "/assets/images/defaults/user_avatar.jpg",
+  // SmartContractAddressProxy: "0xCe4C8b5B7C080D0C7067007B3b96A147FF19d8CF",
   SmartContractAddress: "0x39c2b3934f3Ef3253DB40D6D0A11E74b2e290AB1",
   // SmartContractAddressv1: "0xbAc3A78304fe902e87Bdf13F234947FCCb7879fe",
   TOKEN_BASE_URL: process.env.NEXT_PUBLIC_ENV_BASE_URL + "metadata/tokens",
