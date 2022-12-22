48604a509695f9cf805bddc665736ffb82ca651e
M-Marcel <39571328+M-Marcel@users.noreply.github.com>
2022-12-22 15:57:40 +0100

    gas price experiment

--- src/pages/buy-view-nft/[id].tsx
+++ src/pages/buy-view-nft/[id].tsx
@@ -209,7 +209,7 @@ const ViewNft = () => {
           itemDetail.item.token_id,
           {
             value: price,
-            gasPrice: 20000000,
+            // gasPrice: 20000000,
             // gasPrice: 3124913238,
           }
         );
@@ -231,7 +231,7 @@ const ViewNft = () => {
           item_base_uri,
           {
             value: price,
-            gasPrice: 20000000,
+            // gasPrice: 20000000,
             // gasPrice: 908462167791,
             // maxFeePerGas:18462167791,
             // baseFee: 18462167791 
