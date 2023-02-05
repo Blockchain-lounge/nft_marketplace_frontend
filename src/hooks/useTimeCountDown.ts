5200815bd41cfe3711c317cccb0c3a18515f1dd3
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-02-05 15:30:21 +0100

    fixes

--- src/hooks/useTimeCountDown.ts
+++ src/hooks/useTimeCountDown.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export const useTimeCountDown = (date = "January 6, 2023") => {
+export const useTimeCountDown = (date: string) => {
   const [time, setTime] = useState({
     days: 0,
     hours: 0,
@@ -17,14 +17,37 @@ export const useTimeCountDown = (date = "January 6, 2023") => {
       // Calculate the time left
       const timeLeft = endDate - currentTime;
       // Calculate the days, hours, minutes, and seconds left
+      let remainingDays = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      let remainingHours = Math.floor(
+        (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let remainingMinutes = Math.floor(
+        (timeLeft % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      let remainingSeconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+      if (
+        remainingDays < 0 &&
+        remainingHours < 0 &&
+        remainingMinutes < 0 &&
+        remainingSeconds < 0
+      ) {
+        setTime({
+          ...time,
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0,
+        });
+        return;
+      }
+
       setTime({
         ...time,
-        days: Math.floor(timeLeft / (1000 * 60 * 60 * 24)),
-        hours: Math.floor(
-          (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        ),
-        minutes: Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
-        seconds: Math.floor((timeLeft % (1000 * 60)) / 1000),
+        days: remainingDays,
+        hours: remainingHours,
+        minutes: remainingMinutes,
+        seconds: remainingSeconds,
       });
     }, 1000);
     return () => clearInterval(interval);
