d61ffb6f0dbbe4dc9f55228b5ef5a7806da3c0a8
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-02-07 16:13:14 +0100

    fixes

--- src/components/molecules/SwapCard.tsx
+++ src/components/molecules/SwapCard.tsx
@@ -9,7 +9,10 @@ import {
 } from "../../components/atoms/vectors";
 import { toast, ToastContainer } from "react-toastify";
 import { Button } from "../atoms";
-import { swapEthforWEth } from "@/src/functions/onChain/generalFunction";
+import {
+  getWalletBalance,
+  swapEthforWEth,
+} from "@/src/functions/onChain/generalFunction";
 import { swapWEthforEth } from "@/src/functions/onChain/generalFunction";
 import { getWalletWEthBalance } from "@/src/functions/onChain/generalFunction";
 import { connectedAccount } from "@/src/functions/onChain/authFunction";
@@ -19,7 +22,8 @@ interface IPropsSwap {
   setEthValue: Dispatch<SetStateAction<number>>;
   wETHvalue: string;
   setWETHvalue: Dispatch<SetStateAction<string>>;
-  handleEthSwap: () => void;
+  handleShowModal: Dispatch<SetStateAction<boolean>>;
+  showModal: boolean;
 }
 
 const SwapCard = ({
@@ -27,10 +31,13 @@ const SwapCard = ({
   wETHvalue,
   setEthValue,
   setWETHvalue,
-  handleEthSwap,
+  handleShowModal,
 }: IPropsSwap) => {
   const [activeOfferTab, SetActiveOfferTab] = useState(0);
   const [isTransloading, setIsTransLoading] = useState(false);
+  const [walletAddress, setWalletAddress] = useState(
+    "0xdE8cFsgre5y454754h545uu5u4u5u1C79"
+  );
   const offerTab = [
     { text: "Swap for wETH", icon: SwapIcon },
     { text: "Deposit Crypto", icon: SwapIcon },
@@ -44,15 +51,22 @@ const SwapCard = ({
   const handleWETHvalue = (e: ChangeEvent<HTMLInputElement>) => {
     setWETHvalue(e.target.value);
   };
+
   const handleWEthSwap = () => {
     setIsTransLoading((prev) => !prev);
     if (ethValue != 0) {
       connectedAccount().then((response) => {
         if (response !== null) {
-          getWalletWEthBalance(response).then((balance) => {
-            if (ethValue < balance) {
-              swapEthforWEth(ethValue);
-              setIsTransLoading((prev) => !prev);
+          getWalletBalance(response).then((balance) => {
+            const currentEthBal = balance.split(" ")[0];
+            if (ethValue < Number(currentEthBal)) {
+              swapEthforWEth(ethValue).then((res) => {
+                toast.success(
+                  "eth successfully converted to wEth, Please proceed with your transaction"
+                );
+                handleShowModal((prev) => !prev);
+                setIsTransLoading((prev) => !prev);
+              });
               return;
             } else {
               toast("Insufficent ETH");
@@ -72,6 +86,17 @@ const SwapCard = ({
       setIsTransLoading((prev) => !prev);
       return;
     }
+  };
+
+  const handleCopyToClipboard = () => {
+    navigator.clipboard
+      .writeText(walletAddress)
+      .then(() => {
+        toast.success("Your wallet address has been copied successfully");
+      })
+      .catch((error) => {
+        toast.error("Failed to copy to clipboard: ", error);
+      });
   };
 
   return (
@@ -151,12 +176,13 @@ const SwapCard = ({
             </div>
             <div className="flex flex-col md:flex-row gap-y-6 md:gap-y-0 gap-x-6 mt-12 w-full">
               <span className="md:w-[80%] font-bold py-4 pl-6 rounded-lg bg-bg-6">
-                0xdE8cFsgre5y454754h545uu5u4u5u1C79
+                {walletAddress}
               </span>
               <Button
                 title="Copy"
                 wt="  w-full md:w-[40%]"
                 isDisabled={isTransloading}
+                onClick={handleCopyToClipboard}
               />
             </div>
           </div>
